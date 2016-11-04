436ac030658c6703fa81d8742f55ca2499daa41f
Spencer Wood <spencercwood@gmail.com>
2016-11-04 00:56:34 -0600

    Remove console statements, simplify the code

--- index.js
+++ index.js
@@ -10,33 +10,9 @@ app.post('/:appName', (req, res) => {
   }
   res.send('deployment triggered');
   const { imageName, flags } = config[appName];
-  const pull = `docker pull ${imageName}`;
-  exec(pull, (err, stdout, stderr) => {
-    console.log(pull);
-    if (err) {
-      console.log(err);
-      return;
-    }
-    console.log(stdout);
-    console.log(stderr);
-    const rm = `docker rm -f ${appName} || true`;
-    exec(rm, (err, stdout, stderr) => {
-      console.log(rm);
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log(stdout);
-      console.log(stderr);
-      const run = `docker run -d --name ${appName} ${flags} ${imageName}`;
-      exec(run, (err, stdout, stderr) => {
-        if (err) {
-          console.log(err);
-          return;
-        }
-        console.log(stdout);
-        console.log(stderr);
-      });
+  exec(`docker pull ${imageName}` () => {
+    exec(`docker rm -f ${appName} || true`, () => {
+      exec(`docker run -d --name ${appName} ${flags} ${imageName}`);
     });
   });
 });
