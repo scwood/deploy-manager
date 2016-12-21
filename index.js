b144bd6f0b3f7e1b8791a16d4ad599f23168b4b4
Spencer Wood <spencercwood@gmail.com>
2016-12-20 22:02:30 -0600

    Print out which port the service is listening on

--- index.js
+++ index.js
@@ -17,4 +17,7 @@ app.post('/:appName', (req, res) => {
   });
 });
 
-app.listen(process.env.PORT || 3000);
+const port = process.env.port || 3000;
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
